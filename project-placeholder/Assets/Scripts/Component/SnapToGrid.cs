4d4d16e9c9f5bfe56ea126b69d3b89e99bdb909c
Emil Hultcrantz <90456354+Frequinzy@users.noreply.github.com>
2022-05-01 14:34:10 +0200

    Rename script to SnapToGrid

--- project-placeholder/Assets/Scripts/Component/SnapToGrid.cs
+++ project-placeholder/Assets/Scripts/Component/SnapToGrid.cs
@@ -1,16 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Element : MonoBehaviour {
+public class SnapToGrid : MonoBehaviour {
     public HexCoordinates hexPos;
 
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
-
-    // Update is called once per frame
     void Update() {
         Vector3 pos = transform.position;
         hexPos = HexCoordinates.FromPosition(pos);
